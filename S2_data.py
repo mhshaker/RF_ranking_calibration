80be722536183a6cc176cd39eeecd803ae98f17b
mhshaker <mhshaker71@gmail.com>
2022-12-28 15:36:53 +0100

    Systematic test that RFrank is slightly better than RFprob

--- S2_data.py
+++ S2_data.py
@@ -10,95 +10,110 @@ from sklearn.isotonic import IsotonicRegression
 from sklearn.calibration import calibration_curve
 import matplotlib.pyplot as plt
 
+tree_rank_tau = []
+rf_rank_tau = []
+rf_prob_tau = []
+
 seed = 0
-np.random.seed(seed)
+for seed in range(10):
+    print("seed ", seed)
+    np.random.seed(seed)
 
-calib = False
-kendal = True
-
-
-### Synthetic data generation
-samples = 10000
-
-X, tp = make_regression(samples) # make regression data
-y = np.where(tp>0, 1, 0) # create classification labels by setting a threshold
-
-### spliting data to train calib and test
-test_size = 0.4
-x_train_calib, x_test, y_train_calib, y_test = train_test_split(X, y, test_size=test_size, shuffle=True, random_state=seed)
-x_train, x_calib, y_train, y_calib = train_test_split(x_train_calib, y_train_calib, test_size=0.5, shuffle=True, random_state=seed) 
-_, _, tp_train_calib, tp_test = train_test_split(X, tp, test_size=test_size, shuffle=True, random_state=seed)
-_, _, tp_train, tp_calib = train_test_split(x_train_calib, tp_train_calib, test_size=0.5, shuffle=True, random_state=seed) 
-
-### training the IRRF
-irrf = IR_RF(n_estimators=100, random_state=seed)
-irrf.fit(x_train, y_train)
-
-if kendal:
-
-    # ranking and getting prob for x_test
-    x_test_rank = irrf.rank(x_test, class_to_rank=1)
-    x_test_prob = irrf.predict_proba(x_test, laplace=1)[:, 1]
-    x_test_rank_tree = irrf.rank(x_test, class_to_rank=1, return_tree_rankings=True)[0]
-
-    # sorting x_test based on ranking, prob, and true rankings
-    rank_sort_index = np.argsort(x_test_rank, kind="stable")
-    prob_sort_index = np.argsort(x_test_prob, kind="stable")
-    true_sort_index = np.argsort(tp_test, kind="stable")
-    tree_sort_index = np.argsort(x_test_rank_tree, kind="stable")
-
-    tp_test_rank_sort = tp_test[rank_sort_index]
-    tp_test_prob_sort = tp_test[prob_sort_index]
-    tp_test_true_sort = tp_test[true_sort_index]
-    tp_test_tree_sort = tp_test[tree_sort_index]
-
-    ### ranking performance of the IRRF compared to true rankings
-    tau, p_value = kendalltau(tp_test_true_sort, tp_test_tree_sort)
-    print("true vs tree_rank tau", tau)
-
-    tau, p_value = kendalltau(tp_test_true_sort, tp_test_rank_sort)
-    print("true vs rank tau", tau)
-
-    tau, p_value = kendalltau(tp_test_true_sort, tp_test_prob_sort)
-    print("true vs RFprob tau", tau)
-
-if calib:
-    ### calibration and ECE plot
-    rf_p_calib = irrf.predict_proba(x_calib, laplace=0)
-    rf_p_test = irrf.predict_proba(x_test, laplace=0)
-
-    iso_rf = IsotonicRegression().fit(rf_p_calib[:,1], y_calib)
-    rf_cp_test = iso_rf.predict(rf_p_test[:,1])
+    calib = False
+    kendal = True
 
 
-    x_calib_rank = irrf.rank(x_calib, class_to_rank=1)
-    x_test_rank = irrf.rank(x_test, class_to_rank=1)
+    ### Synthetic data generation
+    samples = 10000
 
-    iso = IsotonicRegression().fit(x_calib_rank, y_calib) 
-    irrf_cp_test = iso.predict(x_test_rank)
+    X, tp = make_regression(samples) # make regression data
+    y = np.where(tp>0, 1, 0) # create classification labels by setting a threshold
 
-    iso_true = IsotonicRegression().fit(tp_calib, y_calib)
-    true_cp_test = iso_true.predict(tp_test)
+    ### spliting data to train calib and test
+    test_size = 0.4
+    x_train_calib, x_test, y_train_calib, y_test = train_test_split(X, y, test_size=test_size, shuffle=True, random_state=seed)
+    x_train, x_calib, y_train, y_calib = train_test_split(x_train_calib, y_train_calib, test_size=0.5, shuffle=True, random_state=seed) 
+    _, _, tp_train_calib, tp_test = train_test_split(X, tp, test_size=test_size, shuffle=True, random_state=seed)
+    _, _, tp_train, tp_calib = train_test_split(x_train_calib, tp_train_calib, test_size=0.5, shuffle=True, random_state=seed) 
+
+    ### training the IRRF
+    irrf = IR_RF(n_estimators=100, random_state=seed)
+    irrf.fit(x_train, y_train)
+
+    if kendal:
+
+        # ranking and getting prob for x_test
+        x_test_rank = irrf.rank(x_test, class_to_rank=1)
+        x_test_prob = irrf.predict_proba(x_test, laplace=1)[:, 1]
+        x_test_rank_tree = irrf.rank(x_test, class_to_rank=1, return_tree_rankings=True)[0]
+
+        # sorting x_test based on ranking, prob, and true rankings
+        rank_sort_index = np.argsort(x_test_rank, kind="stable")
+        prob_sort_index = np.argsort(x_test_prob, kind="stable")
+        true_sort_index = np.argsort(tp_test, kind="stable")
+        tree_sort_index = np.argsort(x_test_rank_tree, kind="stable")
+
+        tp_test_rank_sort = tp_test[rank_sort_index]
+        tp_test_prob_sort = tp_test[prob_sort_index]
+        tp_test_true_sort = tp_test[true_sort_index]
+        tp_test_tree_sort = tp_test[tree_sort_index]
+
+        ### ranking performance of the IRRF compared to true rankings
+        tau, p_value = kendalltau(tp_test_true_sort, tp_test_tree_sort)
+        tree_rank_tau.append(tau)
+        # print("true vs tree_rank tau", tau)
 
 
-    # sig = _SigmoidCalibration().fit(x_calib_rank, y_calib)
-    # irrf_cp_test_sig = iso.predict(x_test_rank)
+        tau, p_value = kendalltau(tp_test_true_sort, tp_test_rank_sort)
+        rf_rank_tau.append(tau)
+        # print("true vs rank tau", tau)
 
-    fop, mpv = calibration_curve(y_test, rf_p_test[:,1], n_bins=10)
-    fop_iso, mpv_iso = calibration_curve(y_test, rf_cp_test, n_bins=10)
-    fop_irrf, mpv_irrf = calibration_curve(y_test, irrf_cp_test, n_bins=10)
-    fop_true, mpv_true = calibration_curve(y_test, true_cp_test, n_bins=10)
-    # fop_irrf_sig, mpv_irrf_sig = calibration_curve(y_test, irrf_cp_test_sig, n_bins=10)
+        tau, p_value = kendalltau(tp_test_true_sort, tp_test_prob_sort)
+        rf_prob_tau.append(tau)
+        # print("true vs RFprob tau", tau)
+
+    if calib:
+        ### calibration and ECE plot
+        rf_p_calib = irrf.predict_proba(x_calib, laplace=0)
+        rf_p_test = irrf.predict_proba(x_test, laplace=0)
+
+        iso_rf = IsotonicRegression().fit(rf_p_calib[:,1], y_calib)
+        rf_cp_test = iso_rf.predict(rf_p_test[:,1])
 
 
-    # plot perfectly calibrated
-    plt.plot([0, 1], [0, 1], linestyle='--')
-    # plot model reliability
-    plt.plot(mpv, fop, marker='.', label="RF")
-    plt.plot(fop_iso, mpv_iso, marker='.', label="RF+iso")
-    plt.plot(fop_irrf, mpv_irrf, marker='.', label="RF+rank+ios", c="black")
-    plt.plot(fop_true, mpv_true, marker='.', label="RF+true+ios", c="red")
-    # plt.plot(fop_irrf_sig, mpv_irrf_sig, marker='.', label="RF+rank+sig", c="blue")
-    plt.legend()
-    plt.show()
-    # plt.savefig("calib_plot.png")
+        x_calib_rank = irrf.rank(x_calib, class_to_rank=1)
+        x_test_rank = irrf.rank(x_test, class_to_rank=1)
+
+        iso = IsotonicRegression().fit(x_calib_rank, y_calib) 
+        irrf_cp_test = iso.predict(x_test_rank)
+
+        iso_true = IsotonicRegression().fit(tp_calib, y_calib)
+        true_cp_test = iso_true.predict(tp_test)
+
+
+        # sig = _SigmoidCalibration().fit(x_calib_rank, y_calib)
+        # irrf_cp_test_sig = iso.predict(x_test_rank)
+
+        fop, mpv = calibration_curve(y_test, rf_p_test[:,1], n_bins=10)
+        fop_iso, mpv_iso = calibration_curve(y_test, rf_cp_test, n_bins=10)
+        fop_irrf, mpv_irrf = calibration_curve(y_test, irrf_cp_test, n_bins=10)
+        fop_true, mpv_true = calibration_curve(y_test, true_cp_test, n_bins=10)
+        # fop_irrf_sig, mpv_irrf_sig = calibration_curve(y_test, irrf_cp_test_sig, n_bins=10)
+
+
+        # plot perfectly calibrated
+        plt.plot([0, 1], [0, 1], linestyle='--')
+        # plot model reliability
+        plt.plot(mpv, fop, marker='.', label="RF")
+        plt.plot(fop_iso, mpv_iso, marker='.', label="RF+iso")
+        plt.plot(fop_irrf, mpv_irrf, marker='.', label="RF+rank+ios", c="black")
+        plt.plot(fop_true, mpv_true, marker='.', label="RF+true+ios", c="red")
+        # plt.plot(fop_irrf_sig, mpv_irrf_sig, marker='.', label="RF+rank+sig", c="blue")
+        plt.legend()
+        plt.show()
+        # plt.savefig("calib_plot.png")
+
+
+print("true vs tree_rank tau", np.array(tree_rank_tau).mean())
+print("true vs rank tau", np.array(rf_rank_tau).mean())
+print("true vs RFprob tau", np.array(rf_prob_tau).mean())
--- test.py
+++ test.py
@@ -1,5 +1,4 @@
-from scipy import stats
 x1 = [1,2,3,4,5]
 x2 = [1,2,3,4,5]
-tau, p_value = stats.kendalltau(x1, x2)
-print("tau", tau)
+
+print("mean", x1.mean())
