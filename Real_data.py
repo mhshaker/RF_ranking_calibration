69605586f51f7c32aa1fff53ac2bb37de7bc8775
mhshaker <mhshaker71@gmail.com>
2023-01-11 16:21:39 +0100

    IRRF calib test on real data. On spambase IRRF is better than ISO but worse than Sig

---
+++ Real_data.py
@@ -0,0 +1,88 @@
+
+import numpy as np
+np.set_printoptions(edgeitems=30, linewidth=100000, 
+    formatter=dict(float=lambda x: "%.3g" % x))
+from sklearn.datasets import make_regression
+from scipy.stats import multivariate_normal
+from sklearn.model_selection import train_test_split
+from estimators.IR_RF_estimator import IR_RF
+from sklearn.isotonic import IsotonicRegression
+from sklearn.calibration import calibration_curve
+from CalibrationM import confidance_ECE, convert_prob_2D
+import Data.data_provider as dp
+from sklearn.calibration import _SigmoidCalibration
+
+runs = 1
+n_estimators=100
+
+plot_bins = 10
+test_size = 0.4
+
+calib = True
+
+ECE_rf_list = []
+ECE_sig_list = []
+ECE_iso_list = []
+ECE_irrf_list = []
+ECE_irrf_sig_list = []
+
+seed = 0
+X, y = dp.load_data("spambase")
+
+for seed in range(runs):
+    print("seed ", seed)
+    np.random.seed(seed)
+
+    ### spliting data to train calib and test
+    x_train_calib, x_test, y_train_calib, y_test = train_test_split(X, y, test_size=test_size, shuffle=True, random_state=seed)
+    x_train, x_calib, y_train, y_calib = train_test_split(x_train_calib, y_train_calib, test_size=0.5, shuffle=True, random_state=seed) 
+
+    ### training the IRRF
+    irrf = IR_RF(n_estimators=n_estimators, random_state=seed)
+    irrf.fit(x_train, y_train)
+
+    if calib:
+        ### calibration and ECE plot
+
+        # random forest probs
+        rf_p_calib = irrf.predict_proba(x_calib, laplace=0)
+        rf_p_test = irrf.predict_proba(x_test, laplace=0)
+
+        # sigmoid calibration on RF
+        sig_rf = _SigmoidCalibration().fit(rf_p_calib[:,1], y_calib)
+        rf_cp_sig_test = sig_rf.predict(rf_p_test[:,1])
+
+        # ISO calibration on RF
+        iso_rf = IsotonicRegression().fit(rf_p_calib[:,1], y_calib)
+        rf_cp_test = iso_rf.predict(rf_p_test[:,1])
+
+        # Ranking with the RF
+        x_calib_rank = irrf.rank(x_calib, class_to_rank=1, train_rank=True)
+        x_test_rank = irrf.rank_refrence(x_test, class_to_rank=1)
+
+        # RF ranking + Sig
+        sig_rank = _SigmoidCalibration().fit(x_calib_rank, y_calib)
+        irrf_cp_sig_test = sig_rank.predict(x_test_rank)
+
+        # RF ranking + ISO
+        iso_rank = IsotonicRegression().fit(x_calib_rank, y_calib) 
+        irrf_cp_test = iso_rank.predict(x_test_rank)
+
+        ece_rf = confidance_ECE(rf_p_test, y_test, bins=plot_bins)
+        ece_sig = confidance_ECE(convert_prob_2D(rf_cp_sig_test), y_test, bins=plot_bins)
+        ece_iso = confidance_ECE(convert_prob_2D(rf_cp_test), y_test, bins=plot_bins)
+        ece_irrf = confidance_ECE(convert_prob_2D(irrf_cp_test), y_test, bins=plot_bins)
+        ece_irrf_sig = confidance_ECE(convert_prob_2D(irrf_cp_sig_test), y_test, bins=plot_bins)
+
+        ECE_rf_list.append(ece_rf)
+        ECE_sig_list.append(ece_sig)
+        ECE_iso_list.append(ece_iso)
+        ECE_irrf_list.append(ece_irrf)
+        ECE_irrf_sig_list.append(ece_irrf_sig)
+
+if calib:
+    print("normal ece   ", np.array(ECE_rf_list).mean())
+    print("sig ece      ", np.array(ECE_sig_list).mean())
+    print("iso ece      ", np.array(ECE_iso_list).mean())
+    print("IRRF iso ece ", np.array(ECE_irrf_list).mean())
+    print("IRRF sig ece ", np.array(ECE_irrf_sig_list).mean())
