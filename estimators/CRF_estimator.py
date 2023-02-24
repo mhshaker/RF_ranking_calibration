c458fc5c011cbdb20f33214a8a960e4e79f28763
Masoud <amshaker79@gmail.com>
2023-02-24 13:20:30 +0330

    CRF calibrator vectorized

--- estimators/CRF_estimator.py
+++ estimators/CRF_estimator.py
@@ -17,23 +17,18 @@ class CRF_calib(BaseEstimator, ClassifierMixin):
 
     def predict(self, X):
 
-        X_2d = convert_prob_2D(X)
-        # Check if fit has been called
-        # check_is_fitted(self)
-
         self.r = self.sig.predict(X)
-
-        max_idx = np.argmax(arr, axis=1)
-        # arg min
-        X_2d[max_idx] += self.r * (1-arr[max_idx])
-        X_2d[min_inx] += arr[1-max_idx] = arr[1-max_idx] * (1-self.r)
         
-        for arr in X_2d:
-            # if pi = Max({p1,..., pk})
-            max_idx = np.argmax(arr)
-            arr[max_idx] = arr[max_idx] + self.r * (1-arr[max_idx])
-            # otherwise
-            arr[1-max_idx] = arr[1-max_idx] * (1-self.r)
+        X_2d = convert_prob_2D(X)        
+
+        max_idx = np.argmax(X_2d, axis=1)
+        min_idx = np.argmin(X_2d, axis=1)
+
+        max_indices = list(range(len(X_2d))), max_idx
+        min_indices = list(range(len(X_2d))), min_idx
+
+        X_2d[max_indices] += self.r * (1-X_2d[max_indices])
+        X_2d[min_indices] *= (1-self.r)
 
         y = X_2d
 
--- test.py
+++ test.py
@@ -8,9 +8,9 @@ max_index = np.argmax(x, axis=1)
 
 r = 5
 
-x[list(range(len(x))), max_index] += r
+max_indices = list(range(len(x))), max_index
+
+x[max_indices] += r
 
 print("x", x)
 print("max_index", max_index)
-
-x[_,_]
