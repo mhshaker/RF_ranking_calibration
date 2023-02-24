0b1e3deda7cf4e2c9584bc51b9783bc73210de4f
Masoud <amshaker79@gmail.com>
2023-02-24 12:10:15 +0330

    CRF calibrator completed

--- estimators/CRF_estimator.py
+++ estimators/CRF_estimator.py
@@ -7,26 +7,21 @@ from sklearn.calibration import _SigmoidCalibration
 
 class CRF_calib(BaseEstimator, ClassifierMixin):
 
-    def __init__(self, r=0.5):
-        # r can be between 0 and 1
-        # including 0 and 1
-        # not including 1, if we want the AUC not to be affected
-        self.r = r
-
     def fit(self, X, y): # X,y are calibration dataset
 
         # [Goal] set the value of "r"
         # learn _SigmoidCalibration "a" and "b"
         # optimize _SigmoidCalibration with brier_score_loss (from sklearn.metrics import brier_score_loss)
-
         self.sig = _SigmoidCalibration().fit(X, y)
+        
+        return self
 
+    def predict(self, X):
 
+        # Check if fit has been called
+        check_is_fitted(self)
 
-        # Check that X and y have correct shape
-        X, y = check_X_y(X, y)
-        # Store the classes seen during fit
-        self.classes_ = unique_labels(y)
+        self.r = self.sig.predict(X)
 
         # if pi = Max({p1,..., pk})
         max_idx = np.argmax(X)
@@ -36,22 +31,5 @@ class CRF_calib(BaseEstimator, ClassifierMixin):
         X[max_idx+1:] = X[max_idx+1:] * (1-self.r)
 
         self.X_ = X
-        self.y_ = y
-        # Return the classifier
-        return self
 
-    def predict(self, X):
-
-        r = self.sig.predict(X)
-
-        # max(X) + r
-        # rest(X) - r
-
-        # Check if fit has been called
-        check_is_fitted(self)
-
-        # Input validation
-        X = check_array(X)
-
-        closest = np.argmin(euclidean_distances(X, self.X_), axis=1)
-        return self.y_[closest]
+        return self.X_
