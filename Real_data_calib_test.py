bd1d56ca515ae195269a029a8759d4ac46b8e9fd
Masoud <amshaker79@gmail.com>
2023-03-01 16:31:50 +0330

    Venn estimator imported

--- Real_data_calib_test.py
+++ Real_data_calib_test.py
@@ -21,6 +21,7 @@ from sklearn.metrics import brier_score_loss
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import roc_curve, auc
 import heapq
+from estimators.Venn_estimator import Venn_calib
 
 runs = 1
 n_estimators=100
@@ -43,71 +44,6 @@ seed = 0
 calib_methods = ["RF", "Platt" , "ISO", "Rank", "CRF"]
 metrics = ["acc", "auc", "brier", "ece"]
 data_list = ["spambase"]
-
-class Venn_calib():
-
-    def fit(self, X, y):
-        
-        preds = np.argmax(X, axis=1)
-        classes = np.unique(preds)
-        n_samples = len(preds)
-        classes_map = {}
-        max_lower, max_upper = [], []
-        final_probs = []
-
-        # create a map
-        # keys are classes
-        # values ore lists of (prediction,label) pairs
-        for c in classes:
-            classes_map[c] = []
-
-        for i in range(n_samples):
-            classes_map[preds[i]].append((preds[i], y[i]))
-
-        # iterate on pairs of each class and compute lower and upper for it
-        for pairs in classes_map.values():
-
-            lower = []
-            upper = []
-            labels_count = {}
-            
-            for c in classes:
-                labels_count[c] = 0
-
-            for pair in pairs:
-                labels_count[pair[1]] += 1
-
-            for c in classes:
-
-                low = labels_count[c] / (len(pairs)+1)
-                up = (labels_count[c]+1) / (len(pairs)+1)
-
-                lower.append(low)
-                upper.append(up)  
-
-            heapq.heapify(lower)
-            heapq.heapify(upper)  
-
-            # get the maximum prob for each class
-            max_lower.append(lower[-1])
-            max_upper.append(upper[-1])
-
-        self.l = np.asarray(max_lower)
-        self.u = np.asarray(max_upper)
-
-        return self
-    
-    
-    def predict(self, X):
-        
-        preds = np.argmax(X, axis=1)
-        classes = np.unique(preds)
-        probs = (self.l + self.u) / 2
-
-        for c in classes:
-            np.place(preds, preds==c, probs[c])
-        
-        return probs
         
 
 for data in data_list:
