fdf595a092e6c5951f961ba3441b9b6d6e64c9ab
Masoud <amshaker79@gmail.com>
2023-03-02 17:42:34 +0330

    Venn estimator corrected

--- Real_data_calib_test.py
+++ Real_data_calib_test.py
@@ -20,7 +20,6 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import brier_score_loss
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import roc_curve, auc
-import heapq
 from estimators.Venn_estimator import Venn_calib
 
 runs = 1
@@ -76,15 +75,6 @@ for data in data_list:
 
         venn = Venn_calib()
         venn.fit(rf_p_calib, y_calib)
-<<<<<<< HEAD
-        probs = venn.prediction(rf_p_test, y_test)
-        print(rf_d_test)
-        print(y_test)
-        print(probs)
-=======
-        print("rf_p_test", rf_p_test.shape)
         probs = venn.predict(rf_p_test)
-        
-        print(probs)
-        print(len(probs))
->>>>>>> d5cc2b4f2b174ad203a05f74db2870958c48ae7d
+
+        print(probs.shape)
--- estimators/Venn_estimator.py
+++ estimators/Venn_estimator.py
@@ -1,5 +1,4 @@
 import numpy as np
-import heapq
 
 
 class Venn_calib():
@@ -10,7 +9,6 @@ class Venn_calib():
         classes = np.unique(preds)
         n_samples = len(preds)
         classes_map = {}
-        # max_lower, max_upper = [], []
 
         # create a map
         # keys are classes
@@ -23,32 +21,27 @@ class Venn_calib():
 
         lower = []
         upper = []
-
+        class_num = 0
         # iterate on pairs of each class and compute lower and upper for it
         for pairs in classes_map.values():
 
             labels_count = {}
             
             for c in classes:
-                labels_count[c] = 0
+                labels_count[c] = 0 
 
             for pair in pairs:
                 labels_count[pair[1]] += 1
 
             for c in classes:
+                if c == class_num:
 
-                low = labels_count[c] / (len(pairs)+1)
-                up = (labels_count[c]+1) / (len(pairs)+1)
+                    low = labels_count[c] / (len(pairs)+1)
+                    up = (labels_count[c]+1) / (len(pairs)+1)
+                    lower.append(low)
+                    upper.append(up) 
 
-                lower.append(low)
-                upper.append(up)  
-
-            # heapq.heapify(lower)
-            # heapq.heapify(upper)  
-
-            # # get the maximum prob for each class
-            # max_lower.append(lower[-1])
-            # max_upper.append(upper[-1])
+            class_num += 1
 
         self.l = np.asarray(lower)
         self.u = np.asarray(upper)
@@ -56,22 +49,24 @@ class Venn_calib():
         return self
     
     
-    def prediction(self, X, y):
+    def predict(self, X):
         
         preds = np.argmax(X, axis=1)
         preds = np.array(preds, dtype=float)
+        n_samples = len(preds)
 
         classes = np.unique(preds)
-        # classes = np.array(classes, dtype=int)
+        classes = np.array(classes, dtype=int)
 
-        probs = (self.l + self.u) / 2 
+        probs = (self.l + self.u) / 2
 
-        n_samples = len(preds)
-        n_classes = len(classes)
-        prob_preds = np.copy(preds)
+        pred_probs = []
 
         for i in range(n_samples):
-            idx = int(n_classes*preds[i] + y[i])
-            prob_preds[i] = probs[idx]
+            if preds[i] == 0:
+                preds[i] = pred_probs.append([probs[0], 1-probs[0]])
+
+            elif preds[i] == 1:
+                preds[i] = pred_probs.append([1-probs[1], probs[1]])
         
-        return prob_preds
+        return np.asarray(pred_probs)
