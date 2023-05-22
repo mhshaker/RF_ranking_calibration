37607159c3ca00aeb1ad6d4c976a49cd9d2af10b
Masoud <amshaker79@gmail.com>
2023-05-22 23:03:41 +0330

    update_intercept method updated

--- estimators/LR_estimator.py
+++ estimators/LR_estimator.py
@@ -2,28 +2,38 @@ from sklearn.linear_model import LogisticRegression
 import numpy as np
 
 
-def sigmoid(z):
-  return (1/ (1+np.exp(-z)))
+def sigmoid(x):
+  return 1 / (1 + np.exp(-x))
 
-def gradient_db(x, y, w, b):
+def compute_gradient_db(X, y, w, b):
+# computes derivative of log-loss function with respect to b
+# using batch gradient descent
+  w = np.squeeze(w)
+  m = X.shape[0]
+  db = 0
 
-  w = np.squeeze(np.asarray(w))
-  db = y - sigmoid(np.dot(w.T, x) + b)
+  for i in range(m):
+       
+     pred = sigmoid(np.dot(X[i], w) + b)  
+     error = pred - y[i]
+     db = db + error
+
+  db = db / m
 
   return db
 
 class LR_u(LogisticRegression):
    
-   def update_intercept(self, X, y, lr, epochs):
+   def update_intercept(self, X, y):
        
        w, b = self.coef_, self.intercept_
-       N = len(X)
+       lr = 0.0001
+       num_iters = 1
 
-       for i in range(0, epochs):
-        for j in range(N):            
-            grad_db = gradient_db(X[j], y[j], w, b)
-            b = b + (lr * grad_db)
+       for i in range(num_iters):
+          db = compute_gradient_db(X, y, w, b)
+          b = b - lr*db
 
-       self.intercept_ = b     
+       self.intercept_ = b
  
        return self
