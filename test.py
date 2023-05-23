c9707f765585afaac8739f5893181053915b309d
mhshaker <mhshaker71@gmail.com>
2023-05-23 13:16:17 +0200

    lr estimator intercept update check

--- test.py
+++ test.py
@@ -1,3 +1,20 @@
 
-for i in range(1000, 1100):
-    print("i", i)
+import Data.data_provider as dp
+from Experiments import core as cal
+from estimators.LR_estimator import LR_u as lr
+from sklearn.linear_model import LogisticRegression
+
+data_name = "spambase"
+X, y = dp.load_data(data_name, ".")
+data = cal.split_train_calib_test(data_name, X, y, 0.3, 0.3, 0)
+
+
+m = lr(random_state=0).fit(data["x_train"], data["y_train"])
+print("intercept before", m.intercept_)
+acc = m.score(data["x_test"], data["y_test"])
+print("acc", acc)
+m = m.update_intercept(data["x_calib"], data["y_calib"])
+print("intercept after ", m.intercept_)
+acc = m.score(data["x_test"], data["y_test"])
+print("acc", acc)
+
