eaf05e32ac9081c8d67edf656fed8863b6b178ef
mhshaker <mhshaker71@gmail.com>
2023-06-29 15:42:24 +0200

    added new dataset. Tested for improvemetns compared with dummy classifier

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
 import numpy as np
 import Data.data_provider as dp
 from estimators.IR_RF_estimator import IR_RF
+from sklearn.dummy import DummyClassifier
 
-data_name = "Customer_Churn"
+data_name = "wilt"
 X, y = dp.load_data(data_name)
-
 rf = IR_RF()
 rf.fit(X, y)
-acc = rf.score(X,y)
+rf_s = rf.score(X,y)
 
 
+# dummy_clf = DummyClassifier(strategy="most_frequent").fit(X, y)
+# d_s = dummy_clf.score(X, y)
 print("acc", np.unique(y))
